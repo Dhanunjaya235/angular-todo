d7ba6524986728dadd20305eb00d0225a83b7c22
Dhanunjaya235 <dhanunjaya.andavarapu@cognine.com>
2024-05-06 14:36:34 +0530

    updated constants

--- src/app/contants.ts
+++ src/app/contants.ts
@@ -1,11 +1,11 @@
-export const baseAPIURL = 'http://localhost:5050/'
+export const baseAPIURL = '';
 
-export const GOOGLE_PROVIDER="GOOGLE"
+export const GOOGLE_PROVIDER = 'GOOGLE';
 
-export const API_NINJA_BASE_URL='https://api.api-ninjas.com/v1/quotes?category='
+export const API_NINJA_BASE_URL = '';
 
-export const API_NINJA_X_API_KEY='6QlmG4YP/whX2+SMO+ZG8g==RxsOHyebaYiJUxxf'
+export const API_NINJA_X_API_KEY = '';
 
-export const VISME_FORM_SCRIPT = 'https://static-bundles.visme.co/forms/vismeforms-embed.js';
+export const VISME_FORM_SCRIPT = '';
 
-export const GOOGLE_CLIENT_ID = '635159642646-38ul4mcuo5rb7r0c87jsbid9n1b2dv77.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_ID = '';
